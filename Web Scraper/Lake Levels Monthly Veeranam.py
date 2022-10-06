ef390c774f31f1ba3b076b8d5b4e792255a009b5
al0210 <77442365+al0210@users.noreply.github.com>
2022-10-06 14:37:25 +0530

    Update Lake Levels Monthly Veeranam.py

--- Web Scraper/Lake Levels Monthly Veeranam.py
+++ Web Scraper/Lake Levels Monthly Veeranam.py
@@ -5,29 +5,39 @@ Created on Tue Oct  4 15:40:50 2022
 @author: al021
 """
 
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
+from bs4 import BeautifulSoup   
+# module - BeautifulSoup will help in scraping data from a given website url
+import requests     
+# requests will allow me to interact with HTTP to get the URL
+import pandas as pd     
+# pandas helps in data structuring
 
 url = "http://123.63.203.150/oldfirstday03.htm"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+# variable - url defined to store the url of the web page we are taking to scrape data from
+page = requests.get(url)        
+# variable - page defined to request the HTTP to get the url stored in the variable 'url'
+soup = BeautifulSoup(page.content, 'html.parser')   
+# object - soup of class BeautifulSoup created to store the page contents of the given url
 
-tables = soup.find_all("table")
+tables = soup.find_all("table") 
+# variable - tables will store all the tables found by the object soup which are present in the html page's contents
 
-lake_table = tables[4]
+lake_table = tables[0]
+# variable - lake_table will store the data of the first table from the variable tables
 lake_rows = lake_table.find_all(['tr'])
+# finds all the rows ('tr' is the rows tag) in the given table
 df_list = []
-for r in range(len(lake_rows)):
+# variable - df_list defined to contain the data from the first table of the given page
+for r in range(len(lake_rows)):     # loop to run through all the rows of the table
     row_values = lake_rows[r].text.split('\n\n\n')
     df_list.append(row_values)
 
-veeranam = pd.DataFrame(df_list)[3:]  
+veeranam = pd.DataFrame(df_list)[3:]            # veeranam will contain data from the third row of the table from the data appended in df_list
 
 for i in veeranam.columns:
-    veeranam[i] = veeranam[i].str.replace('\n\n', '')  
+    veeranam[i] = veeranam[i].str.replace('\n\n', '')           # to replace all '\n\n' with empty space ''
 
-LakeLevel_Veeranam = pd.DataFrame()
+LakeLevel_Veeranam = pd.DataFrame()          # variable - LakeLevel_Veeranam to contain the information of the table
 
 for i in ['Year', 'January','February','March','April','May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']:
     LakeLevel_Veeranam[i] = ""
@@ -47,4 +57,4 @@ LakeLevel_Veeranam['November'] = veeranam.iloc[: , 11]
 LakeLevel_Veeranam['December'] = veeranam.iloc[: , 12]
 
 LakeLevel_Veeranam = LakeLevel_Veeranam.set_index(['Year'])
-LakeLevel_Veeranam.to_csv('Monthly Lake Level Veeranam.csv')
+LakeLevel_Veeranam.to_csv('Monthly Lake Level Veeranam.csv')            # csv file will read the monthly lake level of Chennai's Veeranam reservoir in each year
