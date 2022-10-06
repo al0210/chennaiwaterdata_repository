151cc70f81a1c16499a96eb312ba2eae215bb8f0
al0210 <77442365+al0210@users.noreply.github.com>
2022-10-06 14:30:57 +0530

    Update Lake Levels Monthly Cholavaram.py

--- Web Scraper/Lake Levels Monthly Cholavaram.py
+++ Web Scraper/Lake Levels Monthly Cholavaram.py
@@ -5,29 +5,39 @@ Created on Tue Oct  4 15:03:44 2022
 @author: al021
 """
 
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
+from bs4 import BeautifulSoup   
+# module - BeautifulSoup will help in scraping data from a given website url
+import requests     
+# requests will allow me to interact with HTTP to get the URL
+import pandas as pd     
+# pandas helps in data structuring
 
 url = "http://123.63.203.150/oldfirstday03.htm"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+# variable - url defined to store the url of the web page we are taking to scrape data from
+page = requests.get(url)        
+# variable - page defined to request the HTTP to get the url stored in the variable 'url'
+soup = BeautifulSoup(page.content, 'html.parser')   
+# object - soup of class BeautifulSoup created to store the page contents of the given url
 
-tables = soup.find_all("table")
+tables = soup.find_all("table") 
+# variable - tables will store all the tables found by the object soup which are present in the html page's contents
 
-lake_table = tables[1]
+lake_table = tables[0]
+# variable - lake_table will store the data of the first table from the variable tables
 lake_rows = lake_table.find_all(['tr'])
+# finds all the rows ('tr' is the rows tag) in the given table
 df_list = []
-for r in range(len(lake_rows)):
+# variable - df_list defined to contain the data from the first table of the given page
+for r in range(len(lake_rows)):     # loop to run through all the rows of the table
     row_values = lake_rows[r].text.split('\n\n\n')
     df_list.append(row_values)
 
-chola = pd.DataFrame(df_list)[3:]  
+chola = pd.DataFrame(df_list)[3:]   # chola will contain data from the third row of the table from the data appended in df_list
 
 for i in chola.columns:
-    chola[i] = chola[i].str.replace('\n\n', '')  
+    chola[i] = chola[i].str.replace('\n\n', '')     # to replace all '\n\n' with empty space ''
 
-LakeLevel_Chola = pd.DataFrame()
+LakeLevel_Chola = pd.DataFrame()        # variable - LakeLevel_Chola to contain the information of the table
 
 for i in ['Year', 'January','February','March','April','May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']:
     LakeLevel_Chola[i] = ""
@@ -47,4 +57,4 @@ LakeLevel_Chola['November'] = chola.iloc[: , 11]
 LakeLevel_Chola['December'] = chola.iloc[: , 12]
 
 LakeLevel_Chola = LakeLevel_Chola.set_index(['Year'])
-LakeLevel_Chola.to_csv('Monthly Lake Level Cholavaram.csv')
+LakeLevel_Chola.to_csv('Monthly Lake Level Cholavaram.csv')         # csv file will read the monthly lake level of Chennai's Cholavaram reservoir in each year
