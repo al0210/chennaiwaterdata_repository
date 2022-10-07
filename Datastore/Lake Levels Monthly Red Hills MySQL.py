e41cb7a5260573f9ef5baf06189de7fa8bf945ed
al0210 <77442365+al0210@users.noreply.github.com>
2022-10-07 12:15:29 +0530

    Update Lake Levels Monthly Red Hills MySQL.py

--- Datastore/Lake Levels Monthly Red Hills MySQL.py
+++ Datastore/Lake Levels Monthly Red Hills MySQL.py
@@ -5,20 +5,27 @@ Created on Wed Oct  5 21:22:02 2022
 @author: al021
 """
 
-import csv
-import MySQLdb
+import csv      
+# library to import .csv file which needs to be connected with the database 
+import MySQLdb     
+# importing library - MySQLdb to connect with the database and write the necessary data
 
-lakeleveldb = MySQLdb.connect(host="127.0.0.1", user = "root", password = "", database="chennaiwater_db")
+redhillsdb = MySQLdb.connect(host="127.0.0.1", user = "root", password = "", database="chennaiwater_db") 
+# object - redhillsdb defined to created a connection with the database in the given host, user and password
 
-with open('Monthly Lake Level Red Hills.csv') as csv_file:
-    csvfile = csv.reader(csv_file, delimiter =',')
-    table_value = []
+with open('Monthly Lake Level Red Hills.csv') as csv_file: # to open the .csv that needs to be connected to the database
+    csvfile = csv.reader(csv_file, delimiter =',')      # the .csv which is opened is being read into the object - csvfile
+    table_value = []                 # variable to store the data read from the .csv file
     for row in csvfile:
         row_value = (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12])
-        table_value.append(row_value)
+        table_value.append(row_value)        # the data from the .csv file is appended into table_value rowwise
 
 query = "insert into `table_redhills_monthlylakelevel`(`Year`, `January`, `February`, `March`, `April`, `May`, `June`, `July`, `August`, `September`, `October`, `November`, `December`) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+# to insert the given data into the designated table
 
-dbcursor = lakeleveldb.cursor()
+dbcursor = redhillsdb.cursor()
+# defining the cursor in the database to perform the required executions
 dbcursor.executemany(query, table_value)
-lakeleveldb.commit()
+# executed the operation to store the data in table_value into the database
+redhillsdb.commit()
+# to confirm the changes made into the database
